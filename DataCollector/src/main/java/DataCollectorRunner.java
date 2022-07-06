21d5b4e48dfbc0f28967b40c4becde3d56174a58
behzadnabawi <79253110+behzadnabawi@users.noreply.github.com>
2022-07-06 15:23:46 +0430

    add DataScanner(not completed)

---
+++ DataCollector/src/main/java/DataCollectorRunner.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+
+public class DataCollectorRunner {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+        new DataScanner().run();
+    }
+}
