8a741f8273b5009b176c58819c48424df792714c
behzadnabawi <79253110+behzadnabawi@users.noreply.github.com>
2022-07-07 12:59:50 +0430

    Delete DataCollectorRunner.java

--- DataCollector/src/main/java/DataCollectorRunner.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-
-public class DataCollectorRunner {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
-        new DataScanner().run();
-    }
-}
